8a31dad71dcc2dcbd94103ac0eafe76f09cde26a
dan <kolesov1998@inbox.ru>
2024-07-06 06:28:11 +0300

    4th Step

--- gendiff.js
+++ gendiff.js
@@ -3,6 +3,7 @@
 const { program } = require('commander');
 const fs = require('fs');
 const path = require('path');
+const _ = require('lodash');
 const packageJson = require('./package.json');
 
 // Функция для чтения и парсинга JSON файлов
@@ -10,6 +11,24 @@ const parseFile = (filepath) => {
   const absolutePath = path.resolve(process.cwd(), filepath);
   const fileContent = fs.readFileSync(absolutePath, 'utf-8');
   return JSON.parse(fileContent);
+};
+
+// Функция для сравнения файлов и генерации диффа
+const genDiff = (data1, data2) => {
+  const keys = _.union(Object.keys(data1), Object.keys(data2)).sort();
+  const lines = keys.map((key) => {
+    if (!_.has(data2, key)) {
+      return `  - ${key}: ${data1[key]}`;
+    }
+    if (!_.has(data1, key)) {
+      return `  + ${key}: ${data2[key]}`;
+    }
+    if (data1[key] !== data2[key]) {
+      return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`;
+    }
+    return `    ${key}: ${data1[key]}`;
+  });
+  return `{\n${lines.join('\n')}\n}`;
 };
 
 program
@@ -20,13 +39,9 @@ program
   .action((filepath1, filepath2, options) => {
     const data1 = parseFile(filepath1);
     const data2 = parseFile(filepath2);
-    
-    console.log('File 1 data:', data1);
-    console.log('File 2 data:', data2);
 
-    if (options.format) {
-      console.log('Selected format:', options.format);
-    }
+    const diff = genDiff(data1, data2);
+    console.log(diff);
   });
 
 program.parse(process.argv);
