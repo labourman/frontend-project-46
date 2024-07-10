7ad0fba0b7aa0a73676dad9fb75d5f1ab8589b27
dan <kolesov1998@inbox.ru>
2024-07-10 17:55:32 +0300

    фиксы 6

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,7 +6,7 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('gendiff flat json', () => {
-  const expected = readFile('expected_flat.txt');
+  const expected = readFile('expected_flat.txt').trim();
   const file1 = getFixturePath('file1.json');
   const file2 = getFixturePath('file2.json');
   const result = execFileSync('node', ['gendiff.js', file1, file2]).toString().trim();
